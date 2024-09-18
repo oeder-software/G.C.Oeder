8bdf9babe7b210651ba5176563914968acf59c9c
oeder-software <166632558+oeder-software@users.noreply.github.com>
2024-09-18 14:00:09 +0200

    Add files via upload

---
+++ alifescript.js
@@ -0,0 +1,303 @@
+let alifeContentDanish = [
+  "Ad snoede veje til et mål",
+  "Sammenhængen mellem plantevækst og landbrug",
+  " Betænkning over det spørgsmaal: Hvorledes Frihed og Eiendom kunde forskaffes Bondestanden i de Lande, hvor den fattes begge Dele.",
+  "Betydningen af folkemængdens forøgelse",
+  " Om udstykning af hovedgårde",
+  "Tillæg til Betænkning",
+  "Virkningerne af Oeders skrifter",
+  "Årene 1769 - 1773",
+  "Oldenburg - et fristed",
+  "Mennesket Georg Christian Oeder",
+  "Litteratur",
+  "Efterskrift om en gravskrift",
+];
+let alifeContentEnglish = [
+  "Through winding paths to a goal",
+  "The Relationship Between Plant Growth and Agriculture",
+  "The significance of population increase",
+  " On the subdivision of manor farms",
+  "Supplement to the Report",
+  "The Effects of Oeder's Writings",
+  "The Years 1769 - 1773",
+  "Oldenburg - A Sanctuary",
+  "The Man Georg Christian Oeder",
+  "Literature",
+  "Afterword about an Epitaph",
+];
+let alifeContentGerman = [
+  "Über gewundene Wege zu einem Ziel",
+  "Die Verbindung zwischen Pflanzenwachstum und Landwirtschaft",
+  "Bedenken über die frage: Wie Freiheit und Eigentum der Bauernschaft in den Ländern verschafft werden könnten, in denen beides fehlt.",
+  "Die Bedeutung der Bevölkerungszunahme",
+  "Über die Aufteilung von Hauptgütern",
+  "Anhang zu Bedenken",
+  "Die Auswirkungen von Oeders Schriften",
+  "Die Jahre 1769 - 1773",
+  "Oldenburg - ein Zufluchtsort",
+  "Der Mensch Georg Christian Oeder",
+  "Literatur",
+  "Nachschrift zu einer Grabinschrift",
+];
+
+let alifeContentChapter = [
+  "#chap2",
+  "#chap3",
+  "#chap15",
+  "#chap4",
+  "#chap5",
+  "#chap6",
+  "#chap8",
+  "#chap9",
+  "#chap10",
+  "#chap11",
+  "#chap13",
+  "#chap14",
+];
+let alifeImg = [
+  "wiedewelt1.png",
+  "oeder1.jpg",
+  "jhebernstorff.png",
+  "moltke1.png",
+  "lodde.jpg",
+  "betaenkning.jpg",
+  "zuslag.png",
+  "Schack-Rathlou.jpg",
+  "struensee1.png",
+  "juliane.jpg",
+  "apbernstorff.gif",
+  "Halem11.jpg",
+  "Complex.jpg",
+  "revetlow1.png",
+  "Ludwig.jpg",
+  "caroline.jpg",
+  "multebaer.gif",
+];
+let alifeBio = [
+  "wiedewelt",
+  "oeder",
+  "Bernstorff",
+  "Moltke",
+  "lodde",
+  "betaenkning",
+  "zuslag",
+  "schack",
+  "struensee",
+  "juliane",
+  "apbernstorff",
+  "Halem",
+  "Wessel",
+  "Reventlow",
+  "Ludwig",
+  "caroline",
+  "multebaer",
+];
+
+document
+  .getElementById("img6href")
+  .setAttribute(
+    "href",
+    "https://www.google.dk/books/edition/Bedenken_%C3%BCber_die_Frage_Wie_dem_bauerns/-gluJmCBKOMC?hl=da&gbpv=1&dq=georg+christian+oeder+bedenken+%C3%BCber+die+frage&pg=PA1"
+  );
+document.getElementById("img6href").setAttribute("target", "_blank");
+document.getElementById("img6href").style = "cursor: pointer";
+
+document
+  .getElementById("img7href")
+  .setAttribute(
+    "href",
+    "https://www.google.dk/books/edition/Bedenken_%C3%BCber_die_Frage_Wie_dem_bauerns/-gluJmCBKOMC?hl=da&gbpv=1&dq=georg+christian+oeder+bedenken+%C3%BCber+die+frage&pg=PA75"
+  );
+document.getElementById("img7href").setAttribute("target", "_blank");
+document.getElementById("img7href").style = "cursor: pointer";
+
+document
+  .getElementById("img13href")
+  .setAttribute("href", "https://faculty.etsu.edu/gardnerr/5510/notes/I-3.pdf");
+document.getElementById("img13href").setAttribute("target", "_blank");
+document.getElementById("img13href").style = "cursor: pointer";
+
+// var a = document.createElement("li");
+// a.setAttribute("class", "listelement");
+// a.setAttribute("id", "manu");
+// document.getElementById("list").appendChild(a);
+
+// var a = document.createElement("a");
+// a.setAttribute("id", "drop3");
+// a.innerText = "Manuskripter";
+// document.getElementById("manu").appendChild(a);
+
+// var a = document.createElement("ul");
+// a.setAttribute("class", "drop1");
+// a.setAttribute("id", "manulist");
+// document.getElementById("manu").appendChild(a);
+
+// var a = document.createElement("li");
+// a.setAttribute("class", "listelement");
+// a.setAttribute("id", "manulist1");
+// document.getElementById("manulist").appendChild(a);
+
+//TODO:sæt sprog i if samt sæt href samt lav sidebar
+
+SetAlifeImg(alifeImg);
+var path = parent.location.pathname;
+path = path.split("/").pop();
+path = path.split(".").shift();
+
+if (path == "alifedk") {
+  // var a = document.createElement("a");
+  // a.innerText = "Betænkning";
+  // a.setAttribute("id", "bedenken");
+  // document.getElementById("manulist1").appendChild(a);
+
+  // a.setAttribute("href", "alifedk.html");
+  // document.getElementById("bedenken").appendChild(a);
+
+  var a = document.createElement("a");
+  a.innerText = "Indhold";
+  a.style = "cursor: none; font-size: 20px";
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("a");
+  a.innerText = "Til toppen";
+  a.href = "#dex";
+  var b = a;
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("content").appendChild(a);
+
+  var a = document.createElement("a");
+  a.innerText = "Indhold";
+  a.style = "cursor: none; font-size: 16px";
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("a");
+  a.innerText = "Til toppen";
+  a.href = "#dex";
+  a.setAttribute("onclick", "closeSidebar()");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  SetAlifeContent(alifeContentChapter, alifeContentDanish);
+
+  SetAlifeBio(alifeBio, "DK");
+}
+
+if (path == "alifede") {
+  var a = document.createElement("a");
+  a.innerText = "Inhaltsverzeichnis";
+  a.style = "cursor: none; font-size: 20px";
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("a");
+  a.innerText = "Zu Anfang";
+  a.href = "#dex";
+  var b = a;
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("content").appendChild(a);
+
+  var a = document.createElement("a");
+  a.innerText = "Inhaltsverzeichnis";
+  a.style = "cursor: none; font-size: 20px";
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("a");
+  a.innerText = "Zu Anfang";
+  a.href = "#dex";
+  a.setAttribute("onclick", "closeSidebar()");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  SetAlifeContent(alifeContentChapter, alifeContentGerman);
+  SetAlifeBio(alifeBio, "DE");
+}
+if (path == "alifeen") {
+  var a = document.createElement("a");
+  a.innerText = "ontent";
+  a.style = "cursor: none; font-size: 20px";
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("a");
+  a.innerText = "Page start";
+  a.href = "#dex";
+  var b = a;
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("content").appendChild(a);
+
+  var a = document.createElement("a");
+  a.innerText = "Content";
+  a.style = "cursor: none; font-size: 16px";
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("a");
+  a.innerText = "Page start";
+  a.href = "#dex";
+  a.setAttribute("onclick", "closeSidebar()");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  SetAlifeContent(alifeContentChapter, alifeContentEnglish);
+
+  SetAlifeBio(alifeBio, "EN");
+}
+//https://en.wikipedia.org/wiki/Complex_plane
+function SetAlifeBio(bio, language) {
+  for (let index = 0; index < bio.length; index++) {
+    document
+      .getElementById("bio" + (index + 1))
+      .setAttribute("data", "/bio/" + language + "-" + bio[index] + ".txt");
+  }
+}
+function SetAlifeImg(img) {
+  for (let index = 0; index < img.length; index++) {
+    document
+      .getElementById("img" + (index + 1))
+      .setAttribute("src", "/img/" + img[index]);
+    // document
+    //   .getElementById("img" + (index + 1))
+    //   .setAttribute("alt", alt[index]);
+  }
+}
+function SetAlifeContent(chapter, content) {
+  for (let index = 0; index < content.length; index++) {
+    a = document.createElement("a");
+    a.innerText = content[index];
+    a.href = chapter[index];
+    document.getElementById("content").appendChild(a);
+
+    a = document.createElement("a");
+    a.innerText = content[index];
+    a.href = chapter[index];
+    a.setAttribute("onclick", "closeSidebar()");
+    document.getElementById("sidebarcontent").appendChild(a);
+  }
+}
---
+++ andenkenscript.js
@@ -0,0 +1,416 @@
+let andenkenImg = [
+  "Richardson.jpg",
+  "jhebernstorff.png",
+  "klopstock.jpg",
+  "moltkeorg.gif",
+  "FDanicaFront.gif",
+  "Rousseau.jpg",
+  "multebaer.jpg",
+  "Flora_Svecica.jpg",
+  "linne.jpg",
+  "einleitung.jpg",
+  "hensler.jpg",
+  "christian vii.jpg",
+  "frederik iii.jpg",
+  "frederik iv.jpg",
+  "christian6.jpg",
+  "young.jpg",
+  "betaenkning.jpg",
+  "lodde.jpg",
+  "zuslag.png",
+  "folketaelling1.png",
+  "folketaelling.jpg",
+  "frederik5.jpg",
+  "botaniskhave.jpg",
+  "avnoe.jpg",
+  "struensee.gif",
+  "holstein.jpg",
+  "juliane.jpg",
+  "ferdinand.jpg",
+  "sturz.jpg",
+  "schack.png",
+  "bernstorfforg.gif",
+  "moltke1.png",
+  "arveprins1.jpg",
+  "guldberg.png",
+  "wessel.jpg",
+  "frederik iv.jpg",
+  "wiedewelt1.png",
+  "oedergrav.jpg",
+  "suhm.jpg",
+  "stolberg.jpg",
+];
+
+let andenkenImgAlt = [
+  "Samuel Richardson",
+  "Johan H. E. Bernstorff",
+  "Friedric G. Klpostock",
+  "Adam G. Moltke",
+  "Flora Danica",
+  "Jean-Jacques Rousseau",
+  "multebaer",
+  "Flora Svecica",
+  "Carl von Linné",
+  "einleitung",
+  "Philipp G. Hensler",
+  "Christian VII",
+  "Frederik III",
+  "Frederik IV",
+  "Christian VI",
+  "Arthur Young",
+  "Bedenken über die frage wie dem bauernstande freyheit",
+  "Barthold Johan Lodde",
+  "Zuslag zu Bedenken über die frage wie dem bauernstande freyheit",
+  "Sammlungen zur Geschichte und Staatswissenschaft",
+  "Folketælling",
+  "frederik5",
+  "Botaniskhave",
+  "Avnø",
+  "J. F. Struensee",
+  "Lensgreve von Holstein",
+  "Enkedronning Juliane Marie",
+  "Ferdinand von Braunschweig-Wolfenbüttel",
+  "Helferich Peter Sturz",
+  "Joachim Otto Schack-Rathlou",
+  "Johan Hartvig Ernst Bernstorff",
+  "Adam Gottlob Moltke",
+  "Arveprins Frederik",
+  "Ove Høegh-Guldberg",
+  "Caspar Wessel",
+  "Frederik IV",
+  "Johannes Widewelt",
+  "Gertrudkirchenhof",
+  "Peter Frederik Suhm",
+  "Friedrich Leopold zu Stolberg",
+];
+let andenkenBio = [
+  "richardson",
+  "Bernstorff",
+  "klopstock",
+  "Moltke",
+  "fdanica",
+  "rousseau",
+  "multebaer",
+  "florasvecica",
+  "linne",
+  "urter",
+  "hensler",
+  "ChristianVII",
+  "Frederik3",
+  "frederik4",
+  "christian6",
+  "young",
+  "betaenkning",
+  "lodde",
+  "zuslag",
+  "sammlung",
+  "folketaelling",
+  "frederik5",
+  "botanisk",
+  "avnoe",
+  "struensee",
+  "holstein",
+  "juliane",
+  "ferdinand",
+  "sturz",
+  "schack",
+  "bernstorff",
+  "moltke",
+  "arveprins",
+  "guldberg",
+  "wessel",
+  "frederik4",
+  "wiedewelt",
+  "grav",
+  "suhm",
+  "stolberg",
+  "troldurt",
+];
+let andenkenContentChapter = [
+  "#chap1",
+  "#chap3",
+  "#chap6",
+  "#chap10",
+  "#chap13",
+  "#chap16",
+  "#chap19",
+  "#chap22",
+  "#chap25",
+  "#chap28",
+  "#chap31",
+  "#chap34",
+  "#chap37",
+  "#chap40",
+  "#chap43",
+  "#chap46",
+  "#chap49",
+  "#chap52",
+  "#chap55",
+  "#chap56",
+  "#chap57",
+];
+let andenkenContentEnglish = [
+  "Preface",
+  "Introduction",
+  "Schleswig and the Ericius Family",
+  "Socio-economic Reports",
+  " Technique in Copper Engraving",
+  " Oeder's Reports on Norway's Socioeconomic Situation",
+  "Reflection on Peasant Freedom and Property, 1769",
+  "About Lotteries",
+  "Vaccination of Cattle",
+  "Appointment to Financial Council",
+  "The Finance College is established on May 29, 1771.",
+  "Financial College reformed - Oeder resigns",
+  " Hertug Ferdinand of Brunswick intervenes",
+  "Von Halems' personal acquaintance with Oeder begins",
+  "Characterization of Bernstorff and Moltke",
+  "Establishment of Widow's Funds - in Germany",
+  "The period of Peasant Emancipation in Denmark 1786-1788",
+  "Anti-counterfeiting of banknotes",
+  "On Marriage and Children",
+  "Oeder's Person and Personality",
+  " H. S. E. (HIC SITUS EST)",
+];
+let andenkenContentDanish = [
+  "Forord af E. Willumsen",
+  "Indledning",
+  "Slesvig og familien Ericius",
+  "Samfundsøkonomiske indberetninger",
+  "Teknik ved kobberstik",
+  "Om Norges samfundsøkonomi",
+  "Betænkning over bondens frihed og ejendom 1769",
+  "Om lotterier",
+  "Vaccination af hornkvæg",
+  "Udnævnelse til finansråd",
+  "Finanskollegiet oprettes 29/5 1771.",
+  "Finanskollegiet reformeres",
+  " Hertug Ferdignand af Braunschweig intervenerer",
+  "Von Halems personlige bekendtskab med Oeder indledes",
+  "Karakteristik af Bernstorff og Moltke",
+  "Oprettelsen af enkekasser - i Tyskland",
+  " Bondefrigørelsen i Danmark 1786-1788",
+  " Middel mod forfalskning af pengesedler",
+  "Om ægteskab og børn",
+  " Oeders person og personlighed",
+  "H. S. E. (HIC SITUS EST)",
+];
+let andenkenContentGerman = [
+  "Vorwort von E. Willumsen",
+  "Einleitung",
+  "Sleswig und die familie Ericius",
+  "Volkswirtschaftliche Berichte",
+  "Technik des Kupferstichs",
+  "Über die Volkswirtschaft Norwegens",
+  "Überlegungen zur Freiheit und Eigentum der Bauern 1769",
+  "Über Lotterien",
+  "Vaccination von Hornvieh",
+  "Ernennung zum Finanzrat",
+  "Finanskollegiet wurde am 29. Mai 1771 gegründet.",
+  "Finanskollegiet reformiert ",
+  " Hertug Ferdinand von Braunschweig greift ein",
+  "Von Halems persönliche Bekanntschaft mit Oeder beginnt",
+  "Charakteristika von Bernstorff und Moltke",
+  "Die Gründung von Witwenkassen - in Deutschland",
+  "Bauernbefreiung in Dänemark 1786-1788",
+  " Mittel gegen die Fälschung von Banknoten",
+  "Über Ehe und Kinder",
+  " Oeders Person und Persönlichkeit",
+  "H. S. E. (HIC SITUS EST)",
+];
+SetImgAndenken(andenkenImg, andenkenImgAlt);
+
+document
+  .getElementById("img8href")
+  .setAttribute(
+    "href",
+    "https://archive.org/details/florasuecicaenum01wahl/page/n2/mode/1up?ref=ol&view=theater"
+  );
+document.getElementById("img8href").setAttribute("target", "_blank");
+document.getElementById("img8href").style = "cursor: pointer";
+
+document
+  .getElementById("img10href")
+  .setAttribute(
+    "href",
+    "https://books.google.dk/books?id=YsK_L7n9A1kC&printsec=frontcover&source=gbs_book_other_versions_r&redir_esc=y#v=onepage&q&f=false"
+  );
+document.getElementById("img10href").setAttribute("target", "_blank");
+document.getElementById("img10href").style = "cursor: pointer";
+
+document
+  .getElementById("img17href")
+  .setAttribute("href", "http://www5.kb.dk/e-mat/dod/130020341018.pdf");
+
+document
+  .getElementById("img19href")
+  .setAttribute("href", "http://www5.kb.dk/e-mat/dod/130020341018.pdf");
+
+document.getElementById("img20").setAttribute("src", "/img/folketaelling1.png");
+document
+  .getElementById("img20")
+  .setAttribute("alt", "Sammlungen zur Geschichte und Staatswissenschaft");
+document
+  .getElementById("img20href")
+  .setAttribute(
+    "href",
+    "https://books.google.dk/books?id=KM1AAAAAcAAJ&pg=PA1&hl=da&source=gbs_selected_pages&cad=2#v=onepage&q&f=false"
+  );
+
+//FIXME:
+//TODO:
+var path = parent.location.pathname;
+path = path.split("/").pop();
+path = path.split(".").shift();
+
+if (path == "andenkendk") {
+  var a = document.createElement("a");
+  a.innerText = "Indhold";
+  a.style = "cursor: none; font-size: 20px";
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("a");
+  a.innerText = "Til toppen";
+  a.href = "#dex";
+  var b = a;
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("content").appendChild(a);
+
+  var a = document.createElement("a");
+  a.innerText = "Indhold";
+  a.style = "cursor: none; font-size: 16px";
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("a");
+  a.innerText = "Til toppen";
+  a.href = "#dex";
+  var b = a;
+  a.setAttribute("onclick", "closeSidebar()");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  SetContentAndenken(andenkenContentChapter, andenkenContentDanish);
+
+  SetAndenkenBio(andenkenBio, "DK");
+}
+
+if (path == "andenkende") {
+  var a = document.createElement("a");
+  a.innerText = "Inhaltsverzeichnis";
+  a.style = "cursor: none; font-size: 20px";
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("a");
+  a.innerText = "Zu Anfang";
+  a.href = "#dex";
+  var b = a;
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("content").appendChild(a);
+
+  var a = document.createElement("a");
+  a.innerText = "Inhaltsverzeichnis";
+  a.style = "cursor: none; font-size: 20px";
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("a");
+  a.innerText = "Zu Anfang";
+  a.href = "#dex";
+  var b = a;
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  SetContentAndenken(andenkenContentChapter, andenkenContentGerman);
+  SetAndenkenBio(andenkenBio, "DE");
+}
+if (path == "andenkenen") {
+  var a = document.createElement("a");
+  a.innerText = "ontent";
+  a.style = "cursor: none; font-size: 20px";
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("a");
+  a.innerText = "Page start";
+  a.href = "#dex";
+  var b = a;
+  document.getElementById("content").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("content").appendChild(a);
+
+  var a = document.createElement("a");
+  a.innerText = "Content";
+  a.style = "cursor: none; font-size: 16px";
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("a");
+  a.innerText = "Page start";
+  a.href = "#dex";
+  var b = a;
+  a.setAttribute("onclick", "closeSidebar()");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  a = document.createElement("br");
+  document.getElementById("sidebarcontent").appendChild(a);
+
+  SetContentAndenken(andenkenContentChapter, andenkenContentEnglish);
+
+  SetAndenkenBio(andenkenBio, "EN");
+}
+
+function SetAndenkenBio(bio, language) {
+  for (let index = 0; index < bio.length; index++) {
+    document
+      .getElementById("bio" + (index + 1))
+      .setAttribute("data", "/bio/" + language + "-" + bio[index] + ".txt");
+  }
+}
+function SetImgAndenken(img, alt) {
+  for (let index = 0; index < img.length; index++) {
+    document
+      .getElementById("img" + (index + 1))
+      .setAttribute("src", "/img/" + img[index]);
+    document
+      .getElementById("img" + (index + 1))
+      .setAttribute("alt", alt[index]);
+  }
+}
+function SetContentAndenken(chapter, content) {
+  for (let index = 0; index < content.length; index++) {
+    a = document.createElement("a");
+    a.innerText = content[index];
+    a.href = chapter[index];
+    document.getElementById("content").appendChild(a);
+
+    a = document.createElement("a");
+    a.innerText = content[index];
+    a.href = chapter[index];
+    a.setAttribute("onclick", "closeSidebar()");
+    document.getElementById("sidebarcontent").appendChild(a);
+  }
+}
