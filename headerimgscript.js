eee4f7826c9b59cb199afc231ca7f1ff3d63c0c6
oeder-software <166632558+oeder-software@users.noreply.github.com>
2024-09-18 14:01:12 +0200

    Add files via upload

--- headerimgscript.js
+++ headerimgscript.js
@@ -1,34 +1,60 @@
-function onclick( frameRef ) {
-    return frameRef.contentWindow
-        ? frameRef.contentWindow.document
-        : frameRef.contentDocument
-    }
-    var header = onclick( document.getElementById('frameindex') );
-    if(document.title == "forside" || document.title == "Home" || document.title == "vorderseite"){
-    header.getElementById("bdy").style="background-image:  url(./img/logo.png), url(./img/oederlogohvid200.jpg);";
-    }
-    if(document.title == "letterchede" || document.title == "letterchedk" || document.title == "lettercheen"){
-        header.getElementById("bdy").style="background-image:  url(./img/logo.png), url(./img/leontodon.png);";
-        header.getElementById("headerh2").innerText="Charlotte Hedevig Ericius"
-    }
-    if(document.title == "letterbugdk" || document.title == "letterbugde" || document.title == "letterbugen"){
-        header.getElementById("bdy").style="background-image:  url(./img/logo.png), url(./img/Bugge.jpg);";
-        header.getElementById("headerh2").innerText="Thomas Bugge"
-    }
-    if(document.title == "brevelin" || document.title == "letterlin"){
-        header.getElementById("bdy").style="background-image:  url(./img/logo.png), url(./img/Linne.jpg);";
-        header.getElementById("headerh2").innerText="Carl von Linné"
-    }
-    if( document.title == "familynicodk"){
-        header.getElementById("bdy").style="background-image:  url(./img/logo.png), url(./img/multebær.jpg);";
-        header.getElementById("headerh2").innerText="Efterkommere af Nicolai Gotfried Oeder"
-    }
-    if(document.title == "familynicoen"){
-        header.getElementById("bdy").style="background-image:  url(./img/logo.png), url(./img/multebær.jpg);";
-        header.getElementById("headerh2").innerText="Decendants of Nicolai Gotfried Oeder"
-    }
+function onclick(frameRef) {
+  // return frameRef.contentWindow
+  //   ? frameRef.contentWindow.document
+  //   : frameRef.contentDocument;
+}
+var header = onclick(document.getElementById("frameindex"));
+if (
+  document.title == "forside" ||
+  document.title == "Home" ||
+  document.title == "startseite"
+) {
+  header.querySelector("body").style =
+    "background-image:  url(/img/logo.png), url(/img/oederlogohvid200.jpg);";
+}
+if (
+  document.title == "letterchede" ||
+  document.title == "letterchedk" ||
+  document.title == "lettercheen"
+) {
+  header.getElementById("bdy").style =
+    "background-image:  url(/img/logo.png), url(/img/leontodontrans.png),url(/img/headerbg.png);";
+  header.getElementById("headerh2").innerText = "Charlotte Hedevig Ericius";
+}
+if (
+  document.title == "letterbugdk" ||
+  document.title == "letterbugde" ||
+  document.title == "letterbugen"
+) {
+  header.querySelector("body").style =
+    "background-image:  url(/img/logo.png), url(/img/Bugge.jpg),url(/img/headerbg.png);";
+  header.getElementById("headerh2").innerText = "Thomas Bugge";
+}
+if (
+  document.title == "letterlindk" ||
+  document.title == "letterlinde" ||
+  document.title == "letterlinen"
+) {
+  header.getElementById("bdy").style =
+    "background-image:  url(/img/logo.png), url(/img/linne.jpg),url(/img/headerbg.png);";
+  header.getElementById("headerh2").innerText = "Carl Von Linné";
+}
 
-    if(document.title == "familynicode"){
-        header.getElementById("bdy").style="background-image:  url(./img/logo.png), url(./img/multebær.jpg);";
-        header.getElementById("headerh2").innerText="Nachkommen von Nicolai Gotfried Oeder"
-    }
+if (document.title == "familynicodk") {
+  header.getElementById("bdy").style =
+    "background-image:  url(/img/logo.png), url(/img/multebær.jpg),url(/img/headerbg.png);";
+  header.getElementById("headerh2").innerText =
+    "Efterkommere af Nicolai Gotfried Oeder";
+}
+if (document.title == "familynicoen") {
+  header.getElementById("bdy").style =
+    "background-image:  url(/img/logo.png), url(/img/multebær.jpg),url(/img/headerbg.png);";
+  header.getElementById("headerh2").innerText =
+    "Decendants of Nicolai Gotfried Oeder";
+}
+
+if (document.title == "familynicode") {
+  header.getElementById("bdy");
+  header.getElementById("headerh2").innerText =
+    "Nachkommen von Nicolai Gotfried Oeder";
+}
---
+++ headerscript.js
@@ -0,0 +1,90 @@
+var path = parent.location.pathname;
+path = path.split("/").pop();
+path = path.split(".").shift();
+if (
+  path == "indexdk" ||
+  path == "indexen" ||
+  path == "indexde" ||
+  path == "index"
+) {
+  document.querySelector("body").style =
+    "background-image:  url(/img/logo.png), url(/img/oederlogo.png),url(/img/headerbg.png);";
+}
+if (path == "letterchedk" || path == "letterchede" || path == "lettercheen") {
+  document.querySelector("body").style =
+    "background-image:  url(/img/logo.png), url(/img/leontodontrans.png),url(/img/headerbg.png);";
+  document.getElementById("headerh2").innerText = "Charlotte Hedevig Ericius";
+}
+
+if (path == "letterbugdk" || path == "letterbugde" || path == "letterbugen") {
+  document.querySelector("body").style =
+    "background-image:  url(/img/logo.png), url(/img/bugge.jpg),url(/img/headerbg.png);";
+  document.getElementById("headerh2").innerText = "Thomas Bugge";
+}
+
+if (path == "letterlindk" || path == "letterlinde" || path == "letterlinen") {
+  document.querySelector("body").style =
+    "background-image:  url(/img/logo.png), url(/img/linne.jpg),url(/img/headerbg.png);";
+  document.getElementById("headerh2").innerText = "Carl von Linné";
+}
+if (path == "andenkendk" || path == "andenkende" || path == "andenkenen") {
+  document.querySelector("body").style =
+    "background-image:  url(/img/logo.png), url(/img/halem1.jpg),url(/img/headerbg.png);";
+}
+if (
+  path == "bedenkenprefacedk" ||
+  path == "bedenkenprefacede" ||
+  path == "bedenkenprefaceen"
+) {
+  document.querySelector("body").style =
+    "background-image:  url(/img/logo.png), url(/img/bedenkenforside.png),url(/img/headerbg.png);";
+  // document.querySelector("body").style =
+  //   "background-size: 110px 120px, 110px 110px, cover;";
+}
+switch (path) {
+  case "andenkendk":
+    document.getElementById("headerh2").innerText = "Til minde om Oeder";
+    break;
+  case "andenkende":
+    document.getElementById("headerh2").innerText = "Andenken an Oeder";
+    break;
+  case "andenkenen":
+    document.getElementById("headerh2").innerText = "In Memory of Oeder";
+    break;
+}
+switch (path) {
+  case "alifedk":
+    document.getElementById("headerh2").innerText = "Et liv";
+    break;
+  case "alifeen":
+    document.getElementById("headerh2").innerText = "A Life";
+    break;
+  case "alifede":
+    document.getElementById("headerh2").innerText = "Ein Leben";
+    break;
+}
+switch (path) {
+  case "bedenkenprefacedk":
+    document.getElementById("headerh2").innerText = "Forord til Betænkning";
+    break;
+  case "bedenkenprefaceen":
+    document.getElementById("headerh2").innerText =
+      "Preface to the Consideration";
+    break;
+  case "bedenkenprefacede":
+    document.getElementById("headerh2").innerText = "Vorrede zu Bedenken";
+    break;
+}
+
+if (
+  path == "familynicodk" ||
+  path == "familynicode" ||
+  path == "familynicoen"
+) {
+  document.querySelector("body").style =
+    "background-image:  url(/img/logo.png), url(/img/oederlogo.png),url(/img/headerbg.png);";
+}
+if (path == "alifedk" || path == "alifede" || path == "alifeen") {
+  document.querySelector("body").style =
+    "background-image:  url(/img/logo.png), url(/img/oederlogo.png),url(/img/headerbg.png);";
+}
