a005b9358c47844a66fd23008f783630133d777a
oeder-software <166632558+oeder-software@users.noreply.github.com>
2025-07-29 12:30:02 +0200

    Add files via upload

---
+++ landes-vermessung1script.js
@@ -0,0 +1,10 @@
+let bedenkenContentDanish = [];
+let bedenkenContentEnglish = [];
+let bedenkenContentGerman = [];
+
+let chapter = [];
+let img = "./img/landes-vermessung1.png";
+let link = "https://ds.ub.uni-bielefeld.de/viewer/image/2085009_001/464/";
+SetLinkAndImgToFrontpage(link, img);
+// SetLetterCheContent(chapter, content);
+SetFlora();
---
+++ landes-vermessung2script.js
@@ -0,0 +1,10 @@
+let bedenkenContentDanish = [];
+let bedenkenContentEnglish = [];
+let bedenkenContentGerman = [];
+
+let chapter = [];
+let img = "./img/landes-vermessung2.png";
+let link = "https://ds.ub.uni-bielefeld.de/viewer/image/2085009_002/488/";
+SetLinkAndImgToFrontpage(link, img);
+// SetLetterCheContent(chapter, content);
+SetFlora();
