2b4e7b14041a3e8044604013968f52eef81795e5
oeder-software <166632558+oeder-software@users.noreply.github.com>
2025-07-29 13:02:08 +0200

    Add files via upload

---
+++ bedenkenprefacescript.js
@@ -0,0 +1,11 @@
+let bedenkenContentDanish = [];
+let bedenkenContentEnglish = [];
+let bedenkenContentGerman = [];
+
+let chapter = [];
+let img = "./img/bedenkenforside.png";
+let link =
+  "https://www.google.dk/books/edition/Bedenken_%C3%BCber_die_Frage_wie_dem_Bauerns/LwBCAAAAcAAJ?hl=da&gbpv=1&dq=georg+christian+oeder&printsec=frontcover";
+SetLinkAndImgToFrontpage(link, img);
+
+SetFlora();
